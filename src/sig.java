2349bc0cb15a9d6194697400feccc1712c3b16fa
siniscak <tlsdltkr1026@gmail.com>
2023-05-28 16:54:57 +0900

    sig-m text-m

---
+++ src/sig.java
@@ -0,0 +1,11 @@
+public class sig {
+    public static void main(String[] args) {
+        int number = 1;
+        System.out.println(number);
+        
+        int a = 1;
+        for(int i = 0; i < 10; i++) {
+            System.out.println(a);
+        }
+    }
+}
--- src/text.java
+++ src/text.java
@@ -1,5 +1,6 @@
 public class text {
     public static void main(String[] args) {
         System.out.println("askdfj");
+        System.out.println("ㅁ낭러");
     }
 }
